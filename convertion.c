3eb248af4c0447188d01589757583dd68f8d0dec
gpasques-gh <=>
2025-01-21 18:09:04 +0100

    Premier commit

---
+++ convertion.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+// Fonction convertissant une chaîne de caractères
+// représentant un entier en une chaîne de caractères
+// représentant une valeur héxadécimale
+int d2h(char * intchar, char * hexa) {
+    // Pointeur pour vérifier le format
+    char * ptrFin;
+    // On converti la chaîne de base en entier
+    long entier = strtol(intchar, &ptrFin, 10);
+    // Si le format n'est pas valide
+    if (ptrFin == intchar) {
+        // Fin de la fonction
+        printf("Format invalide.");
+        return 0;
+    } else { // Si le format est valide
+        // Variables
+        char hx[18] = "01234567889ABCDEF"; // Characters hexadécimaux
+        int i = 0; // Indice de la chaine
+        int reste = entier % 16; // Reste de la division
+        int quotient = entier / 16; // Division entière
+        // Entrée de la valeur hexadécimale dans la chaîne
+        hexa[i] = hx[reste];
+        i++;
+        // Tant que l'on peut diviser
+        while (quotient >= 16) {
+            // On réitère l'opération
+            reste = quotient % 16;
+            quotient = quotient / 16;
+            hexa[i] = hx[reste];
+            i++;
+        }
+        // Si le reste est égal à 0
+        if (reste == 0) {
+            reste += 1;
+        }
+        // On ajoute la dernière valeur héxadécimale
+        hexa[i] = hx[reste];
+        // On retourne la chaîne
+        strrev(hexa);
+        // On sort de la fonction en renvoyant 1
+        return 1;
+    }
+}
+
+// Main
+int main(int argc, char* argv[]) {
+    // Variables
+    char* hexa;
+    char* chaine = argv[1];
+    // Appel de la fonction 
+    int resultat = d2h(chaine, hexa);
+    // Affichage du résultat si l'opération a réussi
+    if (resultat == 1) {
+        printf("%s", hexa);
+    }
+    // Fin du programme
+    return 0;
+}
---
+++ etudiants.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+// Structure représentant un étudiant
+struct etudiant {
+    char nom[255];
+    long identifiant;
+    double notes[3];
+};
+
+// Nombre d'élèves de la classe
+int nbEleves;
+
+// Fonction pour instancier un étudiant à l'aide d'un scan
+void scanEtudiant(struct etudiant *etu) {
+    // Saisie des valeurs
+    printf("Veuillez renseigner le nom de l'etudiant : ");
+    scanf("%s", etu -> nom);
+    printf("\nVeuillez renseigner son identifiant : ");
+    scanf("%ld", & etu -> identifiant);
+    printf("\nVeuillez rentrer sa premiere note : ");
+    scanf("%d", & etu -> notes[0]);
+    printf("\nVeuillez rentrer sa deuxieme note : ");
+    scanf("%d", & etu -> notes[1]);
+    printf("\nVeuillez rentrer sa troisieme note : ");
+    scanf("%d", & etu -> notes[2]);
+}
+
+// Fonction pour instancier toute une classe ayant un nombre d'élèves donné
+void scanClasse(struct etudiant* classe, int nbEtudiants) {
+    // On boucle sur toute la classe
+    for (int i = 0; i < nbEtudiants; i++) {
+        // On ajoute l'étudiant
+        struct etudiant etu;
+        scanEtudiant(&etu);
+        classe[i] = etu;
+    }
+}
+
+// Fonction pour instancier toute une classe en finissant par un marqueur donné
+void scanClasseMarqueur(struct etudiant* classe) {
+    // Variables
+    bool fin = false;
+    int i = 0;
+    // On boucle sur toute la classe
+    while (!fin) {
+        // Instanciation de l'étudiant avec scanEtudiant()
+        struct etudiant etu;
+        scanEtudiant(&etu);
+        // On vérifie si l'étudiant n'est pas le marqueur
+        if (strcmp(etu.nom, "0") == 0 &&
+            etu.identifiant == 0 &&
+            etu.notes[0] == 0 &&
+            etu.notes[1] == 0 &&
+            etu.notes[2] == 0) {
+                fin = true; // Si oui la boucle est finie
+            }
+        // Si la boucle n'est pas finie on ajoute l'étudiant à la classe
+        if (!fin) {
+            classe[i] = etu;
+            i++;
+        }        
+    }
+    nbEleves = i; // On actualise le nombre d'élèves
+}
+
+// Fonction affichant un étudiant (nom, identifiant et notes)
+void printEtudiant(struct etudiant etu) {
+    // Affichage de l'étudiant
+    printf("\nL'etudiant renseigne se nomme %s, son identifiant est %ld, et ses notes sont %d, %d, et %d",
+            etu.nom,
+            etu.identifiant,
+            etu.notes[0],
+            etu.notes[1],
+            etu.notes[2]
+    );
+}
+
+// Fonction affichant une classe entière
+void printClasse(struct etudiant* classe, int nbEleves) {
+    // On boucle sur la classe
+    for (int i = 0; i < nbEleves; i++) {
+        // Affichage de l'étudiant
+        printEtudiant(classe[i]);
+    }
+}
+
+int main() {
+
+    // Déclaration de la classe
+    struct etudiant classe[255];
+    // On saisit les élèves de la classe en finissant par le marqueur
+    scanClasseMarqueur(classe);
+    // On affiche la classe finie
+    printClasse(classe, nbEleves);
+
+    return 0;
+}
