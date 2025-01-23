03450f465ea9ff5ac0dc1b09bc75bd48bf812187
gpasques-gh <=>
2025-01-23 18:24:54 +0100

    ajout du programme matDyn.c qui repond à la première question sans le stdin et avec un FILE

--- src/matDyn.c
+++ src/matDyn.c
@@ -10,19 +10,17 @@ char *seps = " ";
 char buffer[255];
 
 // Fonction pour charger les matrices
-void chargerMatrices(int **mat1, int **mat2, FILE *fichier)
+void chargerMatrices(int **mat, FILE *fichier)
 {
     // Variables
-    int i1 = 0, j1 = 0, i2 = 0, j2 = 0;
-    int numMat = 1;
+    int i = 0, j;
     int number;
     // On revient au début du fichier car il a été lu pour la vérification de ce dernier
     rewind(fichier);
     // Boucle sur le lignes du fichier
     while (fgets(buffer, bufferLength, fichier))
     {
-        j1 = 0;
-        j2 = 0;
+        j = 0;
         // Si la ligne n'est pas vide
         if (buffer[0] != '\n')
         {
@@ -38,28 +36,14 @@ void chargerMatrices(int **mat1, int **mat2, FILE *fichier)
                     // On prend le nombre du token
                     number = atol(token);
                     token = strtok(NULL, seps);
-                    // On l'ajoute à la bonne matrice
-                    if (numMat == 1)
-                    {
-                        mat1[i1][j1] = number;
-                        j1++;
-                    }
-                    else if (numMat == 2)
-                    {
-                        mat2[i2][j2] = number;
-                        j2++;
-                    }
+                    // On l'ajoute à la matrice
+                    mat[i][j] = number;
+                    j++;
                 }
             }
-            // On incrémente la ligne de la bonne matrice
-            if (numMat == 1)
-                i1++;
-            else if (numMat == 2)
-                i2++;
+            // On incrémente la ligne de la matrice
+            i++;
         }
-        // Si la ligne est vide et qu'on est à la matrice 1 on passe à la 2
-        else if (buffer[0] == '\n' && numMat == 1)
-            numMat = 2;
     }
 }
 
@@ -79,16 +63,15 @@ void afficherMatrice(int r, int c, int **mat)
 }
 
 // Fonction de vérification du fichier
-bool verifFichier(int r1, int c1, int r2, int c2, FILE *fichier)
+bool verifFichier(int r, int c, FILE *fichier)
 {
     // Variables
-    int i1 = 0, j1 = 0, i2 = 0, j2 = 0, sj1 = 0, sj2 = 0;
+    int i = 0, j = 0, sj = 0;
     int numMat = 1;
     // On boucle sur les lignes du fichier
     while (fgets(buffer, bufferLength, fichier))
     {
-        j1 = 0;
-        j2 = 0;
+        j = 0;
         // Si la ligne n'est pas vide
         if (buffer[0] != '\n')
         {
@@ -125,95 +108,40 @@ bool verifFichier(int r1, int c1, int r2, int c2, FILE *fichier)
                 {
                     // On passe à l'élément suivant
                     token = strtok(NULL, seps);
-                    // On incrémente la colonne de la bonne matrice
-                    if (numMat == 1)
-                        j1++;
-                    else if (numMat == 2)
-                        j2++;
+                    // On incrémente la colonne de la matrice
+                    j++;
                 }
             }
             // Si les colonnes sont valides pour cette ligne
             // On passe à la ligne suivante et on sauvegarde le nombre de colonnes
             // Afin de le comparer avec le nombre de colonne annoncé par l'utilisateur
-            if (numMat == 1 && j1 == c1)
+            if (j == c)
             {
-                i1++;
-                sj1 = j1;
+                i++;
+                sj = j;
             }
-            else if (numMat == 2 && j2 == c2)
-            {
-                i2++;
-                sj2 = j2;
-            }
-            // Sinon on envoie le bon message d'erreur
+            // Sinon on envoie un message d'erreur
             else
             {
-                if (numMat == 1)
-                {
-                    printf("Format des colonnes de la premiere matrice invalide.\n");
-                    return false;
-                }
-                else if (numMat == 2)
-                {
-                    printf("Format des colonnes de la deuxieme matrice invalide.\n");
-                    return false;
-                }
+                printf("Format des colonnes de la matrice invalide.\n");
+                return false;
             }
         }
-        // Si la ligne est vide et que l'on est à la première matrice on passe à la deuxième
-        else if (buffer[0] == '\n' && numMat == 1)
-        {
-            numMat = 2;
-        }
-        // S'il y a de nouvelles lignes vides après la deuxième matrice
+        // S'il y a une ligne vide
         else
         {
-            printf("Format de fichier invalide, plus de deux matrices.\n");
+            printf("Format de fichier invalide, ligne vide trouvée.\n");
             return false;
         }
     }
     // Si les lignes ne correspondent pas aux lignes annoncées par l'utlisateur
-    if (i1 != r1)
+    if (i != r)
     {
         printf("Format des lignes de la premiere matrice invalide.\n");
         return false;
     }
-    if (i2 != r2)
-    {
-        printf("Format des lignes de la deuxieme matrice invalide.\n");
-        return false;
-    }
     // Fin de la fonction
     return true;
-}
-
-// Fonction de multiplication de 2 matrices
-int **multMat(int **mat1, int **mat2, int r1, int c1, int r2, int c2)
-{
-    // On créé la matrice de résultat
-    int **matRes;
-    // On alloue la mémoire pour les lignes
-    matRes = malloc(sizeof(int *) * r1);
-    // On boucle sur les lignes de la première matrice
-    for (int i = 0; i < r1; i++)
-    {
-        // On alloue la mémoire pour les colonnes
-        matRes[i] = malloc(sizeof(int) * c2);
-        // On boucle sur les colonnes de la deuxième matrice
-        for (int k = 0; k < c2; k++)
-        {
-            // On instancie la variable
-            matRes[i][k] = 0;
-            // On boucle sur les colonnes de la première matrice
-            for (int j = 0; j < c1; j++)
-            {
-                // On ajoute le résultat à la variable
-                matRes[i][k] += mat1[i][j] * mat2[j][k];
-            }
-        }
-    }
-    // On renvoit le résultat
-    return matRes;
 }
 
 // Fonction pour libérer la mémoire allouée à une matrice
@@ -230,21 +158,14 @@ void libererMemoire(int **mat, int r)
 int main(int argc, char const *argv[])
 {
     // S'il y a le bon nombre d'arguments
-    if (argc == 6)
+    if (argc == 4)
     {
         // On récupère les arguments de argv
-        int r1 = atol(argv[1]);
-        int c1 = atol(argv[2]);
-        int r2 = atol(argv[3]);
-        int c2 = atol(argv[4]);
-        // On vérifie que l'on peut multiplier les matrices
-        if (r1 != c2 || c1 != r2)
-        {
-            printf("Valeurs incorrectes pour la multiplication de matrices.\n");
-            exit(EXIT_FAILURE);
-        }
+        int r = atol(argv[1]);
+        int c = atol(argv[2]);
+
         // On va chercher le fichier
-        FILE *fichier = fopen(argv[5], "r");
+        FILE *fichier = fopen(argv[3], "r");
         // Si le fichier n'existe pas
         if (fichier == NULL)
         {
@@ -252,7 +173,7 @@ int main(int argc, char const *argv[])
             exit(EXIT_FAILURE);
         }
         // Si le fichier n'est pas valide
-        if (!verifFichier(r1, c1, r2, c2, fichier))
+        if (!verifFichier(r, c, fichier))
         {
             printf("Fermeture du programme.\n");
             exit(EXIT_FAILURE);
@@ -261,32 +182,19 @@ int main(int argc, char const *argv[])
         else
         {
             // On créé les 2 matrices et on alloue la mémoire nécessaire
-            int **mat1 = (int **)malloc(r1 * sizeof(int *));
-            for (int i = 0; i < r1; i++)
-                mat1[i] = (int *)malloc(c1 * sizeof(int *));
+            int **mat = (int **)malloc(r * sizeof(int *));
+            for (int i = 0; i < r; i++)
+                mat[i] = (int *)malloc(c * sizeof(int *));
 
-            int **mat2 = (int **)malloc(r2 * sizeof(int *));
-            for (int i = 0; i < r2; i++)
-                mat2[i] = (int *)malloc(c2 * sizeof(int *));
             // On charge le fichier dans les 2 matrices
-            chargerMatrices(mat1, mat2, fichier);
+            chargerMatrices(mat, fichier);
             // On affiche les 2 matrices
-            printf("Premiere matrice : \n");
-            afficherMatrice(r1, c1, mat1);
-            printf("Deuxieme matrice : \n");
-            afficherMatrice(r2, c2, mat2);
+            printf("Affichage de la matrice : \n");
+            afficherMatrice(r, c, mat);
             // On créé une matrice qui contient le résultat de la multiplication des 2 matrices
-            int **multiplication = multMat(mat1, mat2, r1, c1, r2, c2);
-            // On récupère son nombre de lignes et son nombre de colonnes
-            int rMult = r1;
-            int cMult = c2;
-            // On l'affiche
-            printf("Resultat de la multiplication des 2 matrices : \n");
-            afficherMatrice(rMult, cMult, multiplication);
+
             // On libère la mémoire allouée aux matrices
-            libererMemoire(mat1, r1);
-            libererMemoire(mat2, r2);
-            libererMemoire(multiplication, rMult);
+            libererMemoire(mat, r);
         }
     }
     // Si le nombre d'argument n'est pas le bon
