00d86e72c7ca6c7bc56b29ad1b050935e739c31e
gpasques-gh <germain.pasques4@etu.univ-lorraine.fr>
2025-02-11 10:11:48 +0100

    more modular log function

--- src/log.c
+++ src/log.c
@@ -2,40 +2,50 @@
 #include <signal.h>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
 
 void sig_handling(int s)
 {
     FILE *log = fopen("log.txt", "a");
     time_t t = time(NULL);
     struct tm tm = *localtime(&t);
+    char *str_sig;
 
     if (s == SIGUSR1)
     {
-        fprintf(log, "Signal SIGUSR1 recu le %d/%d/%d a %d:%d:%d.\n", 
-                tm.tm_mday, tm.tm_mon, tm.tm_year,
-                tm.tm_hour, tm.tm_min, tm.tm_sec);
-        printf("Signal SIGUSR1 recu le %d/%d/%d a %d:%d:%d.\n", 
-            tm.tm_mday, tm.tm_mon, tm.tm_year,
-            tm.tm_hour, tm.tm_min, tm.tm_sec);
+        str_sig = "SIGUSR1";
     }
     else if (s == SIGUSR2)
     {
-        fprintf(log, "Signal SIGUSR2 recu le %d/%d/%d a %d:%d:%d.\n", 
-            tm.tm_mday, tm.tm_mon, tm.tm_year,
-            tm.tm_hour, tm.tm_min, tm.tm_sec);
-        printf("Signal SIGUSR2 recu le %d/%d/%d a %d:%d:%d.\n", 
-            tm.tm_mday, tm.tm_mon, tm.tm_year,
-            tm.tm_hour, tm.tm_min, tm.tm_sec);
+        str_sig = "SIGUSR2";
     }
+    else
+    {
+        printf("An unknown signal has reached the signal handling function. This shouldn't happen.\n");
+        fclose(log);
+        exit(EXIT_FAILURE);
+    }
+
+    fprintf(log, "Signal %s recu le %d/%d/%d a %d:%d:%d.\n", str_sig,
+        tm.tm_mday, tm.tm_mon, tm.tm_year,
+        tm.tm_hour, tm.tm_min, tm.tm_sec);
+    
+    printf("Signal %s recu le %d/%d/%d a %d:%d:%d.\n", str_sig,
+        tm.tm_mday, tm.tm_mon, tm.tm_year,
+        tm.tm_hour, tm.tm_min, tm.tm_sec);
 
     fclose(log);
 }
 
 int main(int argc, char const *argv[])
 {
-    
     signal(SIGUSR1, sig_handling);
     signal(SIGUSR2, sig_handling);
-    while(1){}
+
+    while(1)
+    {
+        pause();
+    }
+
     return 0;
 }
